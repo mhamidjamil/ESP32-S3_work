248aa22a9d3adce74e0070a119ad6f43847674bf
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2023-06-11 01:19:44 +0500

    working with SPIFFS

---
+++ SPIFFS/sample_code.ino
@@ -0,0 +1,19 @@
+#include "SPIFFS.h"
+
+void setup() {
+  Serial.begin(115200);
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
+  }
+  File root = SPIFFS.open("/");
+  File file = root.openNextFile();
+  while (file) {
+    Serial.print("FILE: ");
+    Serial.println(file.name());
+    file = root.openNextFile();
+  }
+
+  Serial.println("Done listing files");
+}
+void loop() {}
